e7b01e6d28cfd4d23b45badc8887881bdd51f77c
Philip Ramkeerat <philip.ramkeerat@indt.org.br>
2019-10-17 18:52:43 -0400

    configure crud route in product route js

---
+++ api/routes/product.route.js
@@ -0,0 +1,77 @@
+/**
+ * Using mongose ORM to save datas in Mongo Database
+ */
+const express = require('express');
+const app = express();
+const productRoutes = express.Router();
+
+// Require product model
+let Product = require('../models/Product');
+
+// Store Route
+// Add endpoint
+productRoutes.route('/add').post(function (req, res) {
+  let product = new Product(req.body);
+  product.save()
+    .then(product => {
+      res.status(200).json({ 'Product': 'Product has been added successfully' });
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+});
+
+// get endpoint route
+productRoutes.route('/').get(function (req, res) {
+  Product.find(function (err, products) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.json(products);
+    }
+  });
+});
+
+// Edit route
+productRoutes.route('/edit/:id').get(function (req, res) {
+  let id = req.params.id;
+  Product.findById(id, function (err, product) {
+    res.json(product);
+  });
+});
+
+// Update route
+productRoutes.route('/update/:id').post(function (req, res) {
+  Product.findById(req.params.id, function (err, product) {
+    if (!product)
+      res.status(404).send("Record not found");
+    else {
+      product.ProductName = req.body.ProductName;
+      product.ProductDescription = req.body.ProductDescription;
+      product.ProductPrice = req.body.ProductPrice;
+
+      product.save().then(product => {
+        res.json('Update complete');
+      })
+        .catch(err => {
+          res.status(400).send("unable to update the database");
+        });
+    }
+  });
+});
+
+// Delete route
+productRoutes.route('/delete/:id').get(function (req, res) {
+  Product.findByIdAndRemove({ _id: req.params.id }, function (err, product) {
+    if (err) res.json(err);
+    else res.json('Successfully removed');
+  });
+});
+
+module.exports = productRoutes;
+
+
+
+
+
