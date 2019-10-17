233ceb1e72616f0614add9acc15edd48434f6067
Philip Ramkeerat <philip.ramkeerat@indt.org.br>
2019-10-17 18:12:03 -0400

    configure db name and server cors and mongoose configs

--- api/server.js
+++ api/server.js
@@ -2,9 +2,24 @@ const express = require('express'),
   path = require('path'),
   bodyParser = require('body-parser'),
   cors = require('cors'),
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
+  // Added config DB
+  config = require('./DB');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database connected");
+  },
+  err => {
+    console.log('Cant connect to the database' + err)
+  }
+);
+
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 let port = process.env.PORT || 4000;
 
 const server = app.listen(function () {
