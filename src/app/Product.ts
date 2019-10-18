9f87ae27583787709757da39a9eb09eca2f1089a
Philip Ramkeerat <philip.ramkeerat@indt.org.br>
2019-10-18 11:15:38 -0400

    fixed lint error missing _id declares on class Product

--- src/app/Product.ts
+++ src/app/Product.ts
@@ -1,4 +1,5 @@
 export default class Product {
+  '_id'?: string; // _id mongo objectId to remove lint erros
   ProductName: string;
   ProductDescription: string;
   ProductPrice: number;
--- src/app/product-add/product-add.component.ts
+++ src/app/product-add/product-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductsService } from '../products.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
@@ -8,7 +9,7 @@ import { ProductsService } from '../products.service';
 })
 export class ProductAddComponent implements OnInit {
   angForm: FormGroup;
-  constructor(private fb: FormBuilder, private ps: ProductsService) {
+  constructor(private fb: FormBuilder, private ps: ProductsService, private router: Router) {
     this.createForm();
   }
 
@@ -22,6 +23,7 @@ export class ProductAddComponent implements OnInit {
 
   addProduct(ProductName, ProductDescription, ProductPrice) {
     this.ps.addProduct(ProductName, ProductDescription, ProductPrice);
+    this.router.navigate(['products']);
   }
 
   ngOnInit() {
